0d9298f3746e48e0121d12e95068b7ef07868ae7
Reed Espinosa <reed.espinosa@nasa.gov>
2018-11-08 21:47:35 -0500

    improving angstrom code's handeling of wierd situations

--- GRASP_PythonUtils/miscFunctions.py
+++ GRASP_PythonUtils/miscFunctions.py
@@ -6,10 +6,15 @@ import numpy as np
 def angstrmIntrp(lmbd, tau, lmbdTrgt):
     tau = tau[lmbd.argsort()]
     lmbd.sort()
+    assert (lmbdTrgt >= lmbd.min() and lmbdTrgt <= lmbd.max()), "This function will not extroplate, lmbdTrgt must fall between two values."
+    if lmbdTrgt==lmbd[0]:
+        return tau[0]
+    if lmbdTrgt==lmbd[-1]:
+        return tau[-1]
     frstInd = np.nonzero((lmbd - lmbdTrgt) < 0)[0][-1]
     alpha = angstrm(lmbd[frstInd:frstInd+2], tau[frstInd:frstInd+2])
     return tau[frstInd]*(lmbd[frstInd]/lmbdTrgt)**alpha
     
 def angstrm(lmbd, tau):
-    assert (lmbd.shape[0]==2 and tau.shape[0]==2), "Fitting of more than two values not supported"
+    assert (lmbd.shape[0]==2 and tau.shape[0]==2), "Exactly two values must be provided!"
     return -np.log(tau[0]/tau[1])/np.log(lmbd[0]/lmbd[1])
