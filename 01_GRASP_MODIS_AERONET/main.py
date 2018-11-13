7f3ce87dc52e46380aadd031e47ab3beed935793
Reed Espinosa <reed.espinosa@nasa.gov>
2018-11-13 18:13:30 -0500

    one last general YAML for testing many things

--- 01_GRASP_MODIS_AERONET/main.py
+++ 01_GRASP_MODIS_AERONET/main.py
@@ -21,14 +21,14 @@ from runGRASP import graspDB
 basePath = '/home/respinosa/ReedWorking/'
 binPathGRASP = '/home/respinosa/ReedWorking/Local_Code_MacBook/grasp_open/build/bin/grasp'
 
-maxCPUs = 14 # max simaltanous instances of GRASP
+maxCPUs = 12 # max simaltanous instances of GRASP
 orbHght = 713 # km
 #siteID 9=Wallops, 210=Nauru, 106=Ascension Island, 919=Azores, 58=Cape Verde, Honolulu=97, Amr. Somoa=1185, Midway=278, San Cristobal=243
 siteID = [210, 9, 106, 919, 58, 97, 1185, 278, 243] # can be int or list (not numpy array)
 #siteID = [106, 210, 278] # can be int or list (not numpy array)
 saveNPZ = False
 
-savePath = pathPath = os.path.join(basePath, 'Working/MODAERO_retrievalPickles/MODAERO_GRASPref_updatedYAML6.pkl')
+savePath = pathPath = os.path.join(basePath, 'Working/MODAERO_retrievalPickles/MODAERO_GRASPref_updatedYAMLX.pkl')
 dirPath = pathPath = os.path.join(basePath, 'Remote_Sensing_Projects/GRASP_MODIS/ocean/MOD*.out')
 npzPath = pathPath = os.path.join(basePath, 'Remote_Sensing_Projects/GRASP_MODIS/Ocean_MOD04_L2_C61.out.npz')
 pathYAML = pathPath = os.path.join(basePath, 'Remote_Sensing_Analysis/GRASP_PythonUtils/settings_modis_2lgnrm_optimalNauru.yml')
