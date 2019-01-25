699571296e426ca880bb5158422158f553986ce4
Reed Espinosa <reed.espinosa@nasa.gov>
2019-01-25 10:46:34 -0500

    YAML bug fix and adding option for more/less times per grasp run

--- 01_GRASP_MODIS_AERONET/main.py
+++ 01_GRASP_MODIS_AERONET/main.py
@@ -25,10 +25,10 @@ binPathGRASP = '/home/respinosa/ReedWorking/Local_Code_MacBook/grasp_open/build/
 maxCPUs = 24 # max simaltanous instances of GRASP
 
 #siteID 9=Wallops, 210=Nauru, 106=Ascension Island, 919=Fukue(Japan), 58=Cape Verde, Honolulu=97, Amr. Somoa=1185, Midway=278, Stennis=NOLA
-siteID_global = [210, 9, 106, 919, 58, 97, 1185, 278, 243, 908, 809, 210, 103, 1101, 739, 383, 916, 475, 436, 667] # globaly representative samplw
-siteID_dust = [987, 352, 119, 392, 769, 793, 1031, 187, 962, 469, 471, 470, 543, 461, 467, 906, 1108, 1109, 1114, 507, 256] # DUST SITES
-siteID = np.unique(np.r_[siteID_global, siteID_dust])
-#siteID = [9, 161, 649, 1126, 1149, 518, 510, 268, 196, 514, 59, 1, 430, 221, 1150, 1197, 946, 33, 1077, 1107, 337, 806, 1102, 176, 339, 989, 658, 532, 1156, 746, 961, 981, 748, 316, 666, 485, 940, 922, 285, 33, 126] # LAND SITES
+#siteID_global = [210, 9, 106, 919, 58, 97, 1185, 278, 243, 908, 809, 210, 103, 1101, 739, 383, 916, 475, 436, 667] # globaly representative samplw
+#siteID_dust = [987, 352, 119, 392, 769, 793, 1031, 187, 962, 469, 471, 470, 543, 461, 467, 906, 1108, 1109, 1114, 507, 256] # DUST SITES
+#siteID = np.unique(np.r_[siteID_global, siteID_dust])
+siteID = np.r_[9, 161, 649, 1126, 1149, 518, 510, 268, 196, 514, 59, 1, 430, 221, 1150, 1197, 946, 33, 1077, 1107, 337, 806, 1102, 176, 339, 989, 658, 532, 1156, 746, 961, 981, 748, 316, 666, 485, 940, 922, 285, 33, 126] # LAND SITES
 
 orbHght = 713 # km
 saveNPZ = False
@@ -36,19 +36,20 @@ incldAERO = False # Include AERONET AOD as input to retrieval
 
 #npzPath = os.path.join(basePath, 'Remote_Sensing_Projects/GRASP_MODIS/Land_MYD04_L2_C61_V2c.out.npz') # Use AQUA land Data
 #dirPath = os.path.join(basePath, 'Remote_Sensing_Projects/GRASP_MODIS/landV2/MYD*.outv2') 
-#npzPath = os.path.join(basePath, 'Remote_Sensing_Projects/GRASP_MODIS/Land_MOD04_L2_C61_V2c.out.npz') # Use Terra land Data
-#dirPath = os.path.join(basePath, 'Remote_Sensing_Projects/GRASP_MODIS/landV2/MOD*.outv2') 
+npzPath = os.path.join(basePath, 'Remote_Sensing_Projects/GRASP_MODIS/Land_MOD04_L2_C61_V2c.out.npz') # Use Terra land Data
+dirPath = os.path.join(basePath, 'Remote_Sensing_Projects/GRASP_MODIS/landV2/MOD*.outv2') 
 #npzPath = os.path.join(basePath, 'Remote_Sensing_Projects/GRASP_MODIS/Ocean_MYD04_L2_C61_V2c.out.npz') # Use AQUA ocean Data
 #dirPath = os.path.join(basePath, 'Remote_Sensing_Projects/GRASP_MODIS/oceanV2/MYD*.out') 
-npzPath = os.path.join(basePath, 'Remote_Sensing_Projects/GRASP_MODIS/Ocean_MOD04_L2_C61_V2c.out.npz') # Use Terra ocean Data
-dirPath = os.path.join(basePath, 'Remote_Sensing_Projects/GRASP_MODIS/oceanV2/MOD*.out') 
-fileType = 'ocean' # 'land' or 'ocean'
+#npzPath = os.path.join(basePath, 'Remote_Sensing_Projects/GRASP_MODIS/Ocean_MOD04_L2_C61_V2c.out.npz') # Use Terra ocean Data
+#dirPath = os.path.join(basePath, 'Remote_Sensing_Projects/GRASP_MODIS/oceanV2/MOD*.out') 
+fileType = 'land' # 'land' or 'ocean'
 
-savePath = os.path.join(basePath, 'Working/MODAERO_retrievalPickles/TerraOcean_GLOBALandDUST_YAMLXXX_3lgnrm_V2c.pkl')  # Save results here
-pathYAML = os.path.join(basePath, 'Remote_Sensing_Analysis/GRASP_PythonUtils/settings_modis_3lgnrm_optimalOcean.yml') # YAML file to use
-#pathYAML = os.path.join(basePath, 'Remote_Sensing_Analysis/GRASP_PythonUtils/settings_modis_3lgnrm_optimalLand.yml') # YAML file to use
+savePath = os.path.join(basePath, 'Working/MODAERO_retrievalPickles/TerraLand_GLOBALland_YAML6e3cdda_3lgnrm_V2c.pkl')  # Save results here
+#pathYAML = os.path.join(basePath, 'Remote_Sensing_Analysis/GRASP_PythonUtils/settings_modis_3lgnrm_optimalOcean.yml') # YAML file to use
+pathYAML = os.path.join(basePath, 'Remote_Sensing_Analysis/GRASP_PythonUtils/settings_modis_3lgnrm_optimalLand.yml') # YAML file to use
 dirGRASP = False
 #dirGRASP = os.path.join(basePath, 'Remote_Sensing_Analysis/GRASP_testCases') # working path for GRASP, good for debuging
+maxNtPerSeg=300 # how many pixels per grasp run?
 
 
 # --- CODE ---
@@ -57,7 +58,7 @@ if dirGRASP and maxCPUs > 1: # need seperate directories to run parallel
     dirGRASP = False
 
 
-DB = modaeroDB(None, fileType) if saveNPZ else modaeroDB(npzPath, fileType)
+DB = modaeroDB(None, fileType, maxNtPerSeg) if saveNPZ else modaeroDB(npzPath, fileType, maxNtPerSeg)
 
 if DB.aod.shape[0]==0:
     DB.readDIR(dirPath)
