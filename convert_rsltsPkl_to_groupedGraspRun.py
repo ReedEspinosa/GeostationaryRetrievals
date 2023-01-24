e64be3a63196667a9917919161d4d0b13b50081c
Reed Espinosa <reed.espinosa@nasa.gov>
2023-01-24 18:03:48 -0500

    CHECK: 0.47 land and ocean radiances are suspiciously close for first few pixels

--- convert_rsltsPkl_to_groupedGraspRun.py
+++ convert_rsltsPkl_to_groupedGraspRun.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# TODO: This seems to be working okay so far but 0.47 land and ocean radiances are suspiciously close for first few pixels
+
 import sys
 from os import path
 import numpy as np
@@ -12,7 +14,8 @@ pklInputPath = '/Users/wrespino/Synced/RST_CAN-GRASP/AERONET_collocation/ABI16_A
 # pklInputPath = '/Users/wrespino/Synced/RST_CAN-GRASP/AERONET_collocation/ABI16/ABI16_ALM.pkl'
 # pklInputPath = '/Users/wrespino/Synced/RST_CAN-GRASP/AERONET_collocation/ABI17/ABI17_ALM.pkl'
 
-maxT = 60
+maxT = 60 # The maximum number of pixels to pack together in a single run of GRASP
+measLwrBnd = 1e-6 # Minimum measurement values passed to GRASP 
 
 # load the data
 gDB_in = graspDB()
@@ -23,22 +26,26 @@ del gDB_in
 # HINT: We may want to condition siteIDs (i.e. make them integers)
 
 def packPixel(rs, surf='ocean'):
-    print('We still need to right this one...')
-    if np.isnan(rslts[0]['meas_I_'+surf]).all(): return None
-    land_prct = 100 if surf=='land' else 0 
-    pix = pixel(rs['datetime'], 1, 1, s['longitude'], rs['latitude'], rs['masl'], land_prct)
-    # Prep rs against pixel.populateFromRslt; careful of changes in calling scope (will reuse for land)
-    pix.populateFromRslt(rs, dataStage=meas) # a cheap hack would be to store measurements we want (land/ocean) as the fits (actually any string will do)
+    if np.isnan(rs['meas_I_'+surf]).all(): return None # There was no data for this surface type
+    # Create the pixel
+    land_prct = 100 if surf=='land' else 0
+    pix = pixel(rs['datetime'], 1, 1, rs['longitude'], rs['latitude'], rs['masl'], land_prct)
+    # Update units to match GRASP UPDATE THESE VARIABLE NAMES TO COME FROM rs
+    assert np.all(rs['fis']>=0), 'Negative phi detected in MODIS data but GRASP likes phi on the interval [0,360]' # FIXES: (1) comment this line out and accept less accuracy in GRASP RT OR (2) ensure phi<0 => phi=phi+360
+    mu = np.cos(rs['sza']*np.pi/180) # [0] needed b/c sza might have dummyAng at end
+    rs['meas_I_'+surf] = np.maximum(rs['meas_I_'+surf]*mu, measLwrBnd) # MODIS R=L/FO*pi/mu0; GRASP R=L/FO*pi w/ R>1e-6      
+    # Adjust rs variable names and populate pixel observational data
+    rs['meas_'+surf+'_I'] = rs.pop('meas_I_'+surf) # this is format populateFromRslt() can handle; should have just started with it in convert_YingxiFile2_to_rsltsPkl.py
+    pix.populateFromRslt(rs, dataStage='meas_'+surf)
     return pix
 
 print('Sorting rslts by observations datetime...')
 rslts = rslts[np.argsort([r['datetime'] for r in rslts])]
 print('Looping over all AERONET sites...')
 grObjs = []
-grInd['ocean'] = -1
-grInd['land'] = -1
+grInd = {'ocean':-1, 'land':-1}
 for siteID in np.unique([r['AERO_siteID'] for r in rslts]):
-    matchingRsltsInds = [r['AERO_siteID']==sitID for r in rslts] 
+    matchingRsltsInds = [r['AERO_siteID']==siteID for r in rslts] 
     for rslt in rslts[matchingRsltsInds]:
         for surfType in ['ocean', 'land']:
             surfPix = packPixel(rslt, surfType)
@@ -46,7 +53,7 @@ for siteID in np.unique([r['AERO_siteID'] for r in rslts]):
                 if grInd[surfType]<0 or len(grObjs[grInd[surfType]].pixels)==maxT:
                     grInd[surfType] = len(grObjs)
                     grObjs.append(graspRun(verbose=True))
-                grObjs[grInd[surfType]].addPix(oceanPix)
+                grObjs[grInd[surfType]].addPix(surfPix)
         
 # (3) combine (1) and (2) to pack them all into graspRun members of gDB
 
