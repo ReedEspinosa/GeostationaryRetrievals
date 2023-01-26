d8986f06fa7d4d3d7f302149b4ecac098361991f
Reed Espinosa <reed.espinosa@nasa.gov>
2023-01-26 18:48:33 -0500

    bugs resolved, full scale testing and new warning/feedback messages
    bug resolution:
    - what we thought was site_ID was actually instrument_ID
    - other sites seem to have differing radiance at 0.47μm

--- convert_rsltsPkl_to_groupedGraspRun.py
+++ convert_rsltsPkl_to_groupedGraspRun.py
@@ -14,6 +14,15 @@ from runGRASP import graspDB, graspRun, pixel
 pklInputPath = '/Users/wrespino/Synced/RST_CAN-GRASP/AERONET_collocation/ABI16/ABI16_ALM.pkl'
 # pklInputPath = '/Users/wrespino/Synced/RST_CAN-GRASP/AERONET_collocation/ABI17/ABI17_ALM.pkl'
 
+# maxPixPerSite = None # ALL Pixels
+maxPixPerSite = 10
+
+surfTypes = ['ocean', 'land'] # ALL Surfaces
+# surfTypes = ['ocean']
+
+# siteNames = None # ALL Sites
+siteNames = ['Capo_Verde', 'Cape_San_Juan', 'Dakar_Belair']
+
 maxT = 60 # The maximum number of pixels to pack together in a single run of GRASP
 measLwrBnd = 1e-6 # Minimum measurement values passed to GRASP 
 
@@ -39,30 +48,56 @@ def packPixel(rs, surf='ocean'):
     pix.populateFromRslt(rs, dataStage='meas_'+surf)
     return pix
 
+def checkRsltConsistent(rsBase, rsNow, lenDispStr=0):
+    vld = []
+    vld.append(np.isclose(rsBase['latitude'], rsNow['latitude'], atol=0.5))
+    vld.append(np.isclose(rsBase['longitude'], rsNow['longitude'], atol=0.5))
+    vld.append(np.isclose(rsBase['masl'], rsNow['masl'], atol=500))
+    vld.append(rsBase['AERO_siteName'] == rsNow['AERO_siteName'])
+    if np.all(vld): return True
+    frmt = '%s [%d]: (%.2f,%.2f) - %dm'
+    frmtKeys = ['AERO_siteName','AERO_siteID','latitude','longitude','masl']
+    baseTup = tuple(rsBase[key] for key in frmtKeys)
+    nowTup = tuple(rsNow[key] for key in frmtKeys)
+    preStr = '\b'*lenDispStr + 'AERONET site mismatch >> '
+    print((preStr + frmt + ' || ' + frmt) % (baseTup + nowTup), flush=True)
+    return False
+
 print('Sorting rslts by observations datetime...')
 rslts = rslts[np.argsort([r['datetime'] for r in rslts])]
-print('Converting AERONET SITE IDs to np.int64...')
-for rs in rslts: rs['AERO_siteID'] = np.int64(rs['AERO_siteID'])
 print('Looping over all AERONET sites...')
-dispString = 'Packing %5d pixels at %26s' # len is 45 characters
+dispString = 'Packing %5d pixels at %26s' # len is 50 characters
+lenDispStr = 50
 print(dispString % (0,''), end='')
 grObjs = []
-for siteID in np.unique([r['AERO_siteID'] for r in rslts]):
-    # TODO: Switch from AERO_siteID to AERO_siteName, as former seems scrambled for some cases (e.g., ~11th siteID of ABI16)
-    # TODO: Add assert statement checking that all lat/lons within a single site are within ~1°
-    matchingRsltsInds = [r['AERO_siteID']==siteID for r in rslts]
+if siteNames is None: siteNames = np.unique([r['AERO_siteName'] for r in rslts])
+for siteName in siteNames:
+    matchingRsltsInds = [r['AERO_siteName']==siteName for r in rslts]
     siteName = rslts[matchingRsltsInds][0]['AERO_siteName']
-    print('\b'*50 + (dispString % (sum(matchingRsltsInds), siteName)), end='', flush=True)
+    inconsistRslt = False
+    print('\b'*lenDispStr + (dispString % (sum(matchingRsltsInds), siteName)), end='', flush=True)
     grInd = {'ocean':-1, 'land':-1} # trigger creation of new GRASP run specific to this site
-    for rslt in rslts[matchingRsltsInds]:
-        for surfType in ['ocean', 'land']:
+    for rslt in rslts[matchingRsltsInds][0:maxPixPerSite]:
+        inconsistRslt = inconsistRslt or not checkRsltConsistent(rslts[matchingRsltsInds][0], rslt, lenDispStr)
+        for surfType in surfTypes:
             surfPix = packPixel(rslt, surfType)
             if surfPix is not None:
                 if grInd[surfType]<0 or len(grObjs[grInd[surfType]].pixels)==maxT:
                     grInd[surfType] = len(grObjs)
                     grObjs.append(graspRun(verbose=True))
                 grObjs[grInd[surfType]].addPix(surfPix)
-        
+    if inconsistRslt: print(dispString % (0,''), end='')
+# summarize and sanity check results
+Npix = sum(len(gr.pixels) for gr in grObjs)
+print('\b'*lenDispStr + dispString.replace('king','ked ') % (Npix,('%d sites' % len(siteNames)))) # extra space after ked needed to preserve length
+for gr in grObjs:
+    for pix in gr.pixels:
+        assert np.isclose(gr.pixels[0].land_prct, pix.land_prct, atol=1), 'Land and ocean pixels in same run!'
+Nocean = sum([grObj.pixels[0].land_prct < 1 for grObj in grObjs])
+Nland = sum([grObj.pixels[0].land_prct > 99 for grObj in grObjs])
+print('%d land runs and %d ocean runs for a total of %d runs packed' % (Nland, Nocean, len(grObjs)))
+
+
 # (3) combine (1) and (2) to pack them all into graspRun members of gDB
 
 # (4) save results to a pkl... gDB does not have a method for this (if above is fast we could just run here...)
