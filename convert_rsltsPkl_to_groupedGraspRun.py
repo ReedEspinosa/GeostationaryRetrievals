c5b0d7a8c5d1f3d9ac216862e0734c10142c7915
Reed Espinosa <reed.espinosa@nasa.gov>
2024-01-19 21:50:37 -0500

    Final setting for V004

--- convert_rsltsPkl_to_groupedGraspRun.py
+++ convert_rsltsPkl_to_groupedGraspRun.py
@@ -25,15 +25,20 @@ from simulateRetrieval import simulation
 ### SCRIPT INPUTS ###
 pklInputPath = '/discover/nobackup/projects/multipix_geo/AERONET_collocation/ABI16/ABI16_ALM.pkl'
 
-# max[min]PixPerSite = None # ALL Pixels
-maxPixPerSite = 200 # A max of this many land and ocean pixels will be taken at each site (Npixel_land+ocean can be up to 2*maxPixPerSite)
-
-maxPixPerRun = 30 # The maximum number of pixels to pack together in a single run of GRASP
-minPixPerRun = 15 # GRASP runs with fewer than this many pixels will be discarded
+maxPixPerRun = 30 # The maximum number of pixels to pack together in a single run of GRASP (limited by GRASP constants)
 
 # surfTypes = ['ocean', 'land'] # ALL Surfaces
 surfTypes = ['ocean']
 
+## Testing Configuration
+# maxPixPerSite = 2 # A max of this many land and ocean pixels will be taken at each site (Npixel_land+ocean can be up to 2*maxPixPerSite)
+# minPixPerRun = 1 # GRASP runs with fewer than this many pixels will be discarded
+# siteNames = ['Capo_Verde'] # Full list at bottom of RST Zim Note 
+
+## Production Configuration
+# max[min]PixPerSite = None # ALL Pixels
+maxPixPerSite = 200 # A max of this many land and ocean pixels will be taken at each site (Npixel_land+ocean can be up to 2*maxPixPerSite)
+minPixPerRun = 15 # GRASP runs with fewer than this many pixels will be discarded
 # siteNames = None # ALL Sites
 siteNames = ['Capo_Verde', 'Cape_San_Juan', 'Dakar_Belair', 'San_Cristobal_USFQ', 'UCSB', 
              'Catalina', 'CEILAP-Comodoro', 'Halifax', 'Hampton_University', 'La_Jolla',
@@ -50,7 +55,7 @@ yamlPathGRASP = '/discover/nobackup/wrespino/GeostationaryRetrievals/YAML_files/
 savePath = '/discover/nobackup/projects/multipix_geo/GRASP_results/V0_AERONET-sites_ABI-only/TUNING_oceanSites_maxPerSite200_Ocean_V004.pkl'
 rndGuess = False
 showWorkingPath = True # True prints a large amount to console
-dryRun = True # Pack everything up but do not actually run GRASP
+dryRun = False # Pack everything up but do not actually run GRASP
 
 ### END INPUTS – Begin Processing ###
 
@@ -90,7 +95,7 @@ def calc_maxTnow(n, maxT):
 
 print('Loading collocated data from %s' % (path.basename(pklInputPath)))
 gDB_in = graspDB()
-rslts = gDB_in.loadResults(pklInputPath)
+rslts = np.asarray(gDB_in.loadResults(pklInputPath)) # Not great practice, but we do convert back to list at bottom when saving
 del gDB_in
 for var in ['n','k']: # ensure 2D arrays with size 2xNλ for consistency with rslt dict (AERONET retrieve same value for fine/coarse RI)
     if var in rslts[0] and (rslts[0][var].ndim==1 or rslts[0][var].shape[0]==1):
@@ -127,7 +132,7 @@ for siteName in siteNames:
 if minPixPerRun is not None:
     if maxPixPerSite: assert minPixPerRun <= maxPixPerSite, 'minPixPerRun was greater than maxPixPerSite!'
     if maxPixPerRun: assert minPixPerRun <= maxPixPerRun, 'minPixPerRun was greater than maxPixPerRun!'
-    vldRuns = [len(grObj.pixels) > minPixPerRun for grObj in grObjs]
+    vldRuns = [len(grObj.pixels) >= minPixPerRun for grObj in grObjs]
     assert vldRuns.count(True) > 0, 'No runs contained more than minPixPerRun=%d pixels' % minPixPerRun
     if vldRuns.count(False) > 0:
         print('Canceling %d runs with fewer than %d pixels' % (vldRuns.count(False), minPixPerRun))
@@ -157,31 +162,10 @@ else:
     simABI.rsltBck = rsltBck
     print('%d of %d pixels processed successfully' % (sum(~failRun), Npix))
 # del gDB # helpful in large scale processing, but not debugging
-simABI.rsltFwd = rslts[triedPixIDs][~failRun]
+simABI.rsltFwd = list(rslts[triedPixIDs][~failRun])
 if not dryRun: simABI.spectralInterpFwdToBck() # line up fwd with back wavelengths
 simABI.saveSim(savePath, verbose=True)
 print('Simulation object with results saved to:\n%s' % savePath)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
